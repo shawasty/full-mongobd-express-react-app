5339071880aadea06aec153a336cd02810dd3bd9
shawasty <shawasty@gmail.com>
2022-04-05 10:28:29 -0400

    2nd commit

--- controllers/index.js
+++ controllers/index.js
@@ -21,7 +21,7 @@ const getAllItems = async (req, res) => {
     }
   }
 
-  
+
 
 const getItemById = async (req, res) => {
     try {
@@ -33,6 +33,23 @@ const getItemById = async (req, res) => {
         return res.status(404).send('Item with the specified ID does not exists')
     } catch (error) {
         return res.status(500).send(error.message)
+    }
+  }
+
+  const updateItem =  (req, res) => {
+    try {
+        const { id } = req.params;
+         Item.findByIdAndUpdate(id, req.body, { new: true }, (err, item) => {
+            if (err) {
+                res.status(500).send(err);
+            }
+            if (!item) {
+                res.status(500).send('Item not found!');
+            }
+            return res.status(200).json(item)
+        })
+    } catch (error) {
+        return res.status(500).send(error.message);
     }
   }
 
