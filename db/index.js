f30e950378e3ada87fcfaf9c41e2c5a64026ebc5
shawasty <shawasty@gmail.com>
2022-04-05 11:18:00 -0400

    fixed some bugs causing errors in app

--- db/index.js
+++ db/index.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+let MONGODB_URI =
+  process.env.PROD_MONGODB ||
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1:27017/thirdItemsDatabase";
+//Remember this? Remember that you have to name your database. All of the other things are more of a memorization thing.
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1 : 27017/thirdItemsDatabase'
-
-mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
-    console.log('Successfully connected to MongoDB')
+mongoose
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log("Successfully connected to MongoDB");
   })
-  
-      
-  const db = mongoose.connection 
-  
-  module.exports = d
+  .catch((e) => {
+    console.error("Connection error", e.message);
+  });
 
+const db = mongoose.connection;
+
+module.exports = db;
