06ef093086ab95410db9ea9e5be26fc7bd189d47
shawasty <shawasty@gmail.com>
2022-04-05 23:44:56 -0400

    rendered items title

--- client/src/components/routes/Item.js
+++ client/src/components/routes/Item.js
@@ -1,6 +1,11 @@
-import React from 'react';
+// import { useState, useEffect } from 'react';
+// import { NavLink, useNavigate, useParams } from "react-router-dom";
+// import axios from 'axios'
+// import Layout from '../shared/Layout';
 
 const Item = () => {
+ 
+
     return (
         <div>
             item
--- client/src/components/routes/Items.js
+++ client/src/components/routes/Items.js
@@ -1,9 +1,48 @@
-import React from 'react';
+
+
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import axios from 'axios'
+import Layout from '../shared/Layout';
+import { useState, useEffect } from 'react';
 
 const Items = () => {
+    const [items, setItems] = useState([]);
+    
+    const fetchData = async () => {
+        try {
+          const response = await axios('http://localhost:3000/api/items')
+
+          console.log('res', response.data.items)
+          setItems(response.data.items)
+    
+            } catch (error) {
+          console.error(error)
+            }
+        // fetchData()
+    }
+    useEffect(()=>{
+        fetchData()
+    },[])
+
+    // set a variable to map the items as below
+
+    const itemsData = items.map((item)=>{
+        return <li key={item._id}>
+            <NavLink to={`/items/${item._id}`} >{item.title}</NavLink>
+        </li>
+    })
+
+
+
+
+
     return (
         <div>
-            items
+            <h4>items</h4>
+            <ul>
+
+            {itemsData}
+            </ul>
         </div>
     );
 };
