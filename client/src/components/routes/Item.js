3162a147624fc1f9ab5d398e09988ef0a4463799
shawasty <shawasty@gmail.com>
2022-04-06 04:41:26 -0400

    delete function works now

--- client/src/components/routes/Item.js
+++ client/src/components/routes/Item.js
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
-import {  useParams } from "react-router-dom";
+import {  useParams,NavLink, useNavigate } from "react-router-dom";
 import axios from 'axios'
-// import Layout from '../shared/Layout';
+import Layout from '../shared/Layout';
+// import { NavLink } from "react-router-dom";
 
 const Item = () => {
     const [item, setItem] = useState({})
     // sets delete fxns
     const [deleted, setDeleted] = useState(false)
+    let navigate = useNavigate();
 
     const { id } = useParams()
     console.log ('params', id)
@@ -31,9 +33,7 @@ const Item = () => {
             const response = await axios.get(`http://localhost:3000/api/items/${id}`)
             const result = response.data.item;
             console.log('res',result )
-            // const uniqItem = result.find(uniq => uniq._id ===id)
             setItem(result)
-            // console.log('j', uniqItem)
             return result;
         } catch (error) {
             console.log(error)
@@ -41,35 +41,38 @@ const Item = () => {
     }
         fetchData()
     },[])
-    const handleItem = () => {
-        // fetchData(id).then(res => {
-        //     const uniqItem = res.find(uniq => uniq.id === id);
-        //     setItem(uniqItem)
-        //     console.log ('id',uniqItem)
-        // })
-    } 
-   
-
+    
     useEffect(()=>{
         if(!item) {
             return <p>Loading...</p>
         }
     },[item])
 
-    // useEffect(()=>{
-    //     handleItem();
-    // },[])
+    const destroy = ()=>{
+        axios({
+            url: `http://localhost:3000/api/items/${id}`,
+            method: 'DELETE'
+        }).then(()=> setDeleted(true)).catch(console.error)
+    }
 
-       
-    
-        console.log ('params', id)
-
-        console.log('item', item)
+    useEffect(()=>{
+        if (deleted) {
+            return navigate('/')
+        }
+    },[deleted, navigate])
 
     return (
         <div>
-            <p>{item.title}</p>
-            <p>{item.link}</p>
+            <Layout>
+            <h4>{item.title}</h4>
+            <p>LINK:{item.link}</p>
+            <button onClick={()=> destroy()}>Delete Item</button>
+            <NavLink to={`/items/${id}/edit`}>
+                <button>Edit</button>
+            </NavLink>
+            <NavLink to='/items'> Back to all items
+            </NavLink>
+            </Layout>
             
         </div>
     );
